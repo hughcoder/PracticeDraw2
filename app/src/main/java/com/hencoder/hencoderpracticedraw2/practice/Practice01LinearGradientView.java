841620c8a4e22b604007f2e456e32ee9ed0c59ad
chenyw <chenyw@tuya.com>
2018-09-25 18:01:16 +0800

    commit

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -3,13 +3,16 @@ package com.hencoder.hencoderpracticedraw2.practice;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Shader;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice01LinearGradientView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Shader shader = new LinearGradient(100,100,500,500,Color.parseColor("#E91E63"),Color.parseColor("#2196F3"),Shader.TileMode.CLAMP);
 
     public Practice01LinearGradientView(Context context) {
         super(context);
@@ -24,6 +27,13 @@ public class Practice01LinearGradientView extends View {
     }
 
     {
+        /**
+         * LinearGradient(float x0, float y0, float x1, float y1, int color0, int color1, Shader.TileMode tile) 。
+
+         参数：
+         x0 y0 x1 y1：渐变的两个端点的位置
+         color0 color1 是端点的颜色
+         */
         // 用 Paint.setShader(shader) 设置一个 LinearGradient
         // LinearGradient 的参数：坐标：(100, 100) 到 (500, 500) ；颜色：#E91E63 到 #2196F3
     }
@@ -31,7 +41,7 @@ public class Practice01LinearGradientView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        paint.setShader(shader);
         canvas.drawCircle(300, 300, 200, paint);
     }
 }
